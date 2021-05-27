b850b2b37adc57a3e72d10ce29dada04cc9e9243
Muhammad Alfian <pianaee379@gmail.com>
2021-05-27 20:43:42 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+alert("Selamat datang!");
+var lagi = true;
+
+while (lagi) {
+  var nama = prompt("Masukkan nama : ");
+  if (nama) {
+    alert("Halo " + nama + "!");
+  } else if (nama == null) {
+    alert("Anda harus memasukkan nama!");
+  } else {
+    alert("Anda harus memasukkan nama!");
+  }
+  lagi = confirm("Ingin mencoba lagi?");
+}
+alert("Thankyou!");
